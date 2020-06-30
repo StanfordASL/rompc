c6527add3c51a58bd51d90cd12490432b027ed98
Joe Lorenzetti <jlorenze@stanford.edu>
2020-06-30 13:50:59 -0700

    some heatflow problem data

--- models/heatflow/error_heatflow.m
+++ models/heatflow/error_heatflow.m
@@ -4,11 +4,11 @@ path = fileparts(which('error_heatflow.m'));
 [FOM, ROM, CTRL, Z, U, NOISE] = heatflow(false);
 opts.discrete = true;
 
-
 opts.G_method = 'BatchGP';
 opts.Cr_method = 'hyperrect';
 opts.Cw_method = 'hyperrect';
-tau = 2500;
+tau = 2000;
+opts.Xbar_tau = 500;
 opts.normbound_datapath = strcat(path, '/data/NORMBOUND.mat');
 opts.errormats_datapath = strcat(path, '/data/ERRORMATS.mat');
 opts.xbar_datapath = strcat(path, '/data/XBAR.mat');
