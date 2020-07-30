00da04528d9c5be1debf5cf0fbc855d33a58fd3a
Joe Lorenzetti <jlorenze@stanford.edu>
2020-07-30 13:36:05 -0700

    updates to simulator for open loop

--- rompc/simulateROMPC.m
+++ rompc/simulateROMPC.m
@@ -23,19 +23,26 @@ function [DATA] = simulateROMPC(FOM, ROM, CTRL, ROM_CTRL, Zbar, T, X0, opt)
 %       - rompc_dt: for continuous time simulation, discretization time
 %                   used in ROMPC definition (ROMPC.dt)
 %       - save_xf: true to save full state, default = false
+%       - rom_only: true to only simulate the ROM, default = false
 %
 % Returns:
 %   DATA: struct containing simulation data as well as options and
 %         controller query times
 
-nf = size(FOM.Af, 1);
-m = size(FOM.Bf, 2);
-p = size(FOM.Cf, 1);
-o = size(FOM.Hf, 1);
+if ~isfield(opt, 'rom_only')
+    opt.rom_only = false;
+end
 
-if ~isfield(opt, 'save_xf')
+if opt.rom_only || ~isfield(opt, 'save_xf')
     opt.save_xf = false;
 end
+
+if ~opt.rom_only
+    nf = size(FOM.Af, 1);
+end
+m = size(ROM.B, 2);
+p = size(ROM.C, 1);
+o = size(ROM.H, 1);
 
 % Continuous or discrete time
 if isfield(opt, 'continuous')
@@ -59,9 +66,11 @@ if isfield(opt, 'continuous')
     [Ade, Bde, Bwe] = zoh(opt.sim_dt, ROM.A - CTRL.L*ROM.C, ROM.B, CTRL.L);
     
     % Define preconditioner for backward Euler simulation
-    S = speye(nf) - opt.sim_dt*FOM.Af;
-    setup.type = 'nofill';
-    [M1, M2] = ilu(sparse(S), setup);
+    if ~opt.rom_only
+        S = speye(nf) - opt.sim_dt*FOM.Af;
+        setup.type = 'nofill';
+        [M1, M2] = ilu(sparse(S), setup);
+    end
     
 elseif isfield(opt, 'discrete')
     continuous = ~opt.discrete;
@@ -72,41 +81,59 @@ end
 
 % Check what kind of controller was used for ROM
 if isstruct(ROM_CTRL)
-    precompiled = false;
-    rompc = true;
+    % Open loop control
+    if isfield(ROM_CTRL, 'ubar') && isfield(ROM_CTRL, 'xbar')
+        ctrl_type = 'openloop';
+        X0.xbar = ROM_CTRL.xbar(:,1);
+        if size(ROM_CTRL.ubar, 2) == T-1
+            ROM_CTRL.ubar = [ROM_CTRL.ubar, ROM_CTRL.ubar(:,end)];
+        end
+        if ~isfield(ROM_CTRL, 't') && continuous
+            error('Open loop needs time field for continuous time problems.');
+        end
+
+    % ROMPC without precompiled optimizer
+    else
+        ctrl_type = 'rompc';
+    end
 elseif isa(ROM_CTRL, 'optimizer')
-    precompiled = true;
-    rompc = true;
+    ctrl_type = 'rompc_precompiled';
 elseif ismatrix(ROM_CTRL)
-    rompc = false;
+    ctrl_type = 'linearfeedback';
 end
 
-if ~isfield(opt, 'NOISE')
-    opt.NOISE = struct();
-    FOM.Bfw = zeros(nf,1);
+if ~opt.rom_only
+    if ~isfield(opt, 'NOISE')
+        opt.NOISE = struct();
+        FOM.Bfw = zeros(nf,1);
+    else
+        if isfield(opt.NOISE, 'W')
+            mw = size(opt.NOISE.W.A,2);
+            idxUB = find(opt.NOISE.W.A*ones(mw,1) == 1);
+            idxLB = find(opt.NOISE.W.A*ones(mw,1) == -1);
+            wfUB = opt.NOISE.W.b(idxUB);
+            wfLB = -opt.NOISE.W.b(idxLB);
+        end
+        if isfield(opt.NOISE, 'V')
+            idxUB = find(opt.NOISE.V.A*ones(p,1) == 1);
+            idxLB = find(opt.NOISE.V.A*ones(p,1) == -1);
+            vUB = opt.NOISE.V.b(idxUB);
+            vLB = -opt.NOISE.V.b(idxLB);
+        end
+    end
 else
-    if isfield(opt.NOISE, 'W')
-        mw = size(opt.NOISE.W.A,2);
-        idxUB = find(opt.NOISE.W.A*ones(mw,1) == 1);
-        idxLB = find(opt.NOISE.W.A*ones(mw,1) == -1);
-        wfUB = opt.NOISE.W.b(idxUB);
-        wfLB = -opt.NOISE.W.b(idxLB);
-    end
-    if isfield(opt.NOISE, 'V')
-        idxUB = find(opt.NOISE.V.A*ones(p,1) == 1);
-        idxLB = find(opt.NOISE.V.A*ones(p,1) == -1);
-        vUB = opt.NOISE.V.b(idxUB);
-        vLB = -opt.NOISE.V.b(idxLB);
-    end
+    opt.NOISE = struct();
 end
 
-if rompc
+if strcmp(ctrl_type, 'rompc') || strcmp(ctrl_type, 'rompc_precompiled')
     [proj] = correctionProjection(ROM.H, Zbar, opt);
 end
 
 % Containers for data
-z = zeros(o, T);
-u = zeros(m, T);
+if ~opt.rom_only
+    z = zeros(o, T);
+    u = zeros(m, T);
+end
 zbar = zeros(o, T);
 ubar = zeros(m, T);
 tsolver = zeros(1, T);
@@ -117,8 +144,10 @@ else
 end
 
 % Initial conditions
-xf = X0.xf;
-xhat = X0.xhat;
+if ~opt.rom_only
+    xf = X0.xf;
+    xhat = X0.xhat;
+end
 xbar = X0.xbar;
 if continuous
     xbar_k = X0.xbar;
@@ -128,6 +157,7 @@ end
 fprintf('Percent complete:      ');
 prc_cmplt = 0;
 J = 0;
+Jr = 0;
 i = update;
 for t = 1:T
     % Specify some noise terms
@@ -145,22 +175,26 @@ for t = 1:T
     end
     
     % Get measurement and outputs
-    y = FOM.Cf*xf + v;
-    z(:,t) = FOM.Hf*xf;
-    zbar(:,t) = ROM.H*xbar;
-    if opt.save_xf
-        xfdata(:,t) = xf;
+    if ~opt.rom_only
+        y = FOM.Cf*xf + v;
+        z(:,t) = FOM.Hf*xf;
+        if opt.save_xf
+            xfdata(:,t) = xf;
+        end
     end
-       
+    zbar(:,t) = ROM.H*xbar;
+     
     % Solve the optimal control problem
-    if rompc && precompiled
+    if strcmp(ctrl_type, 'rompc_precompiled')
         if ~continuous || (continuous && i == update)
             if continuous
                 xbar = xbar_k; % reset
             end
             [sol, ~, ~, ~, ~, solver] = ROM_CTRL(xbar);
             ubar(:,t) = sol{1};
-            u(:,t) = ubar(:,t) + CTRL.K*(xhat - xbar);
+            if ~opt.rom_only
+                u(:,t) = ubar(:,t) + CTRL.K*(xhat - xbar);
+            end
             
             % Correct for numerical errors if needed
             xbar_k = sol{2};
@@ -171,7 +205,9 @@ for t = 1:T
             i = 1;
         elseif continuous && i ~= update
             ubar(:,t) = sol{1}; % from previous solve (ZOH)
-            u(:,t) = ubar(:,t) + CTRL.K*(xhat - xbar);
+            if ~opt.rom_only
+                u(:,t) = ubar(:,t) + CTRL.K*(xhat - xbar);
+            end
             i = i + 1;
             solver.solvertime = 0;
         end
@@ -183,14 +219,16 @@ for t = 1:T
             xbar = xbar_k;
         end
         
-    elseif rompc && ~precompiled
+    elseif strcmp(ctrl_type, 'rompc')
         if ~continuous || (continuous && i == update)
             if continuous
                 xbar = xbar_k; % reset
             end
             [xopt, uopt, solver] = solveOptimalControl(ROM_CTRL, xbar, opt);
             ubar(:,t) = uopt(:,1);
-            u(:,t) = ubar(:,t) + CTRL.K*(xhat - xbar);
+            if ~opt.rom_only
+                u(:,t) = ubar(:,t) + CTRL.K*(xhat - xbar);
+            end
             
             % Correct for numerical errors if needed
             xbar_k = xopt(:,2);
@@ -200,7 +238,9 @@ for t = 1:T
             i = 1;
         elseif continuous
             ubar(:,t) = uopt(:,1);
-            u(:,t) = ubar(:,t) + CTRL.K*(xhat - xbar);
+            if ~opt.rom_only
+                u(:,t) = ubar(:,t) + CTRL.K*(xhat - xbar);
+            end
             i = i + 1;
             solver.solvertime = 0;
         end
@@ -212,16 +252,45 @@ for t = 1:T
             xbar = xbar_k;
         end
         
-    else
+    elseif strcmp(ctrl_type, 'linearfeedback')
         t0 = tic;
         ubar(:,t) = ROM_CTRL*xbar;
         solver.solvertime = toc(t0);
-        u(:,t) = ubar(:,t) + CTRL.K*(xhat - xbar);
+        if ~opt.rom_only
+            u(:,t) = ubar(:,t) + CTRL.K*(xhat - xbar);
+        end
         if continuous
             xbar = Ad*xbar + Bd*ubar(:,t);
         else
             xbar = ROM.A*xbar + ROM.B*ubar(:,t);
         end
+        solver.problem = 0;
+
+    elseif strcmp(ctrl_type, 'openloop')
+        if continuous
+            time = (t-1)*opt.sim_dt;
+            timenext = t*opt.sim_dt;
+            if timenext > ROM_CTRL.t(end)
+                break;
+            end
+            ubar(:,t) = interp1(ROM_CTRL.t, ROM_CTRL.ubar', time);
+            if ~opt.rom_only
+                u(:,t) = ubar(:,t) + CTRL.K*(xhat - xbar);
+            end
+            xbar = interp1(ROM_CTRL.t, ROM_CTRL.xbar', timenext);
+            xbar = xbar';
+        else
+            if t >= size(ROM_CTRL.ubar, 2)
+                break;
+            end
+            ubar(:,t) = ROM_CTRL.ubar(:,t);
+            if ~opt.rom_only
+                u(:,t) = ubar(:,t) + CTRL.K*(xhat - xbar);
+            end
+            xbar = ROM_CTRL.xbar(:,t+1);
+            xbar = xbar';
+        end       
+        solver.solvertime = 0;
         solver.problem = 0;
     end
     
@@ -232,26 +301,35 @@ for t = 1:T
     end
     tsolver(t) = solver.solvertime;
     
-    % Update the reduced order state estimate
-    if continuous
-        xhat = Ade*xhat + Bde*u(:,t) + Bwe*y;
-    else
-        xhat = ROM.A*xhat + ROM.B*u(:,t) + CTRL.L*(y - ROM.C*xhat);  
+    if ~opt.rom_only
+        % Update the reduced order state estimate
+        if continuous
+            xhat = Ade*xhat + Bde*u(:,t) + Bwe*y;
+        else
+            xhat = ROM.A*xhat + ROM.B*u(:,t) + CTRL.L*(y - ROM.C*xhat);  
+        end
+    
+        % Update the full order (true) system dynamics
+        if continuous
+            [xfsim] = simulateViaBackEuler(FOM.Af, FOM.Bf, xf, opt.sim_dt, u(:,t), zeros(nf,1), M1, M2);
+            xf = xfsim(:,2);
+        else
+            xf = FOM.Af*xf + FOM.Bf*u(:,t) + FOM.Bfw*wf;
+        end
+    
+        % Update cost
+        if continuous
+            J = J + opt.sim_dt*(xf'*FOM.Qf*xf + u(:,t)'*FOM.Rf*u(:,t));
+        else
+            J = J + xf'*FOM.Qf*xf + u(:,t)'*FOM.Rf*u(:,t);
+        end
     end
     
-    % Update the full order (true) system dynamics
+    % Update reduced order cost
     if continuous
-        [xfsim] = simulateViaBackEuler(FOM.Af, FOM.Bf, xf, opt.sim_dt, u(:,t), zeros(nf,1), M1, M2);
-        xf = xfsim(:,2);
+        Jr = Jr + opt.sim_dt*(xbar'*ROM.Q*xbar + ubar(:,t)'*ROM.R*ubar(:,t));
     else
-        xf = FOM.Af*xf + FOM.Bf*u(:,t) + FOM.Bfw*wf;
-    end
-    
-    % Update cost
-    if continuous
-        J = J + opt.sim_dt*(xf'*FOM.Qf*xf + u(:,t)'*FOM.Rf*u(:,t));
-    else
-        J = J + xf'*FOM.Qf*xf + u(:,t)'*FOM.Rf*u(:,t);
+        Jr = Jr + xbar'*ROM.Q*xbar + ubar(:,t)'*ROM.R*ubar(:,t);
     end
     
     if round(100*(t/T)) > prc_cmplt
@@ -261,18 +339,21 @@ for t = 1:T
 end
 fprintf('\n');
 
-DATA.u = u;
-DATA.z = z;
+if ~opt.rom_only
+    DATA.u = u;
+    DATA.z = z;
+    DATA.xf = xfdata;
+end
 DATA.ubar = ubar;
 DATA.zbar = zbar;
-DATA.xf = xfdata;
 DATA.tsolver = tsolver;
 DATA.T = T;
 DATA.X0 = X0;
 DATA.opt = opt;
 DATA.J = J;
+DATA.Jr = Jr;
 if continuous
-    DATA.t = 0:opt.sim_dt:T;
+    DATA.t = opt.sim_dt*(0:T-1);
 end
 
 end
